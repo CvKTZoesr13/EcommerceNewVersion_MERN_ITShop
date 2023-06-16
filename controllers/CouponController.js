eea3b32081dbe6b17f5900ac2750de07d0b40536
CvKTZoesr13 <vanducdo4@gmail.com>
2023-06-16 14:54:58 +0700

    add basic CRUD functionality for coupon

--- controllers/CouponController.js
+++ controllers/CouponController.js
@@ -11,4 +11,46 @@ const createCoupon = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { createCoupon };
+const getAllCoupons = asyncHandler(async (req, res) => {
+  try {
+    const coupons = await Coupon.find();
+    res.json(coupons);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const updateCoupon = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const updatedCoupon = await Coupon.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.json(updatedCoupon);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+const deleteCoupon = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  validateMongoDbId(id);
+  try {
+    const deletedCoupon = await Coupon.findByIdAndDelete(id);
+    if (deletedCoupon != null) {
+      res.json({
+        deletedCoupon: deletedCoupon,
+        message: "This coupon has been deleted!",
+      });
+    } else {
+      res.status(404).json({
+        message: "NotFound",
+      });
+    }
+  } catch (error) {
+    throw new Error(error);
+  }
+});
+
+module.exports = { createCoupon, getAllCoupons, updateCoupon, deleteCoupon };
