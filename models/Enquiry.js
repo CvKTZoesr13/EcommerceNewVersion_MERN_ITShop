5572918daee325f42d2aa8f9ed76be487c9defd0
CvKTZoesr13 <vanducdo4@gmail.com>
2023-06-27 21:47:54 +0700

    handle upload and delete images in cloudinary

---
+++ models/Enquiry.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose"); // Erase if already required
+
+// Declare the Schema of the Mongo model
+var enqSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+    index: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  mobile: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  comment: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    default: "Submitted",
+    enum: ["Submitted", "In progress", "Contacted"],
+  },
+});
+
+//Export the model
+module.exports = mongoose.model("Enquiry", enqSchema);
